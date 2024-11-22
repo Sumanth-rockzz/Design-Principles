57317c2bad355ceac5a310929847452df25f6cc8
Sumanth-rockzz <Sumanthn876@gmail.com>
2024-11-22 22:29:05 +0530

    Builder Pattern

---
+++ BuilderPattern.js
@@ -0,0 +1,55 @@
+// Builder Pattern
+
+
+// Intent:
+// The Builder Pattern simplifies the creation of complex objects by constructing them step-by-step 
+//and separating the construction logic from the representation.
+
+
+//Think of ordering a custom pizza. 
+//You specify step-by-step what toppings and crust you want, and the chef follows the instructions to build it.
+
+
+class Pizza {
+    constructor() {
+      this.toppings = [];
+      this.crust = "regular";
+      this.size = "medium";
+    }
+  }
+  
+  class PizzaBuilder {
+    constructor() {
+      this.pizza = new Pizza();
+    }
+  
+    setCrust(crust) {
+      this.pizza.crust = crust;
+      return this;
+    }
+  
+    setSize(size) {
+      this.pizza.size = size;
+      return this;
+    }
+  
+    addTopping(topping) {
+      this.pizza.toppings.push(topping);
+      return this;
+    }
+  
+    build() {
+      return this.pizza;
+    }
+  }
+  
+  const pizza = new PizzaBuilder()
+    .setCrust("thin")
+    .setSize("large")
+    .addTopping("cheese")
+    .addTopping("pepperoni")
+    .build();
+  
+  console.log(pizza);
+  // Pizza { toppings: [ 'cheese', 'pepperoni' ], crust: 'thin', size: 'large' }
+  
